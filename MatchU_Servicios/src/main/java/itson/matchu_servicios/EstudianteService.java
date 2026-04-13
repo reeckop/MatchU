480a3c5fb6c1153ef773c8b53aaa1b6f895d36b1
DayannaBeltran <carmen.beltran248565@potros.itson.edu.mx>
2026-04-13 12:37:30 -0700

    Cambios en servicios
    En mensaje y en estudiante

--- MatchU_Servicios/src/main/java/itson/matchu_servicios/EstudianteService.java
+++ MatchU_Servicios/src/main/java/itson/matchu_servicios/EstudianteService.java
@@ -1,7 +1,10 @@
 package itson.matchu_servicios;
 
 import itson.daos.EstudianteDAO;
+import itson.daos.MatchDAO;
 import itson.matchu_dominio.models.Estudiante;
+import itson.matchu_dominio.models.Match;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -11,9 +14,11 @@ import java.util.Optional;
 public class EstudianteService {
     
     private final EstudianteDAO estudianteDAO;
+    private final MatchDAO matchDAO;
 
     public EstudianteService() {
         this.estudianteDAO = new EstudianteDAO();
+        this.matchDAO = new MatchDAO();
     }
   
     public Estudiante login(String correo, String contrasena) throws Exception {
@@ -36,14 +41,31 @@ public class EstudianteService {
         if (estudiante.getNombre() == null || estudiante.getNombre().trim().isEmpty()) {
             throw new Exception("El nombre es obligatorio.");
         }
+        if (estudiante.getApellidos() == null || estudiante.getApellidos().trim().isEmpty()) {
+            throw new Exception("Los apellidos son obligatorios.");
+        }
         if (estudiante.getCorreo() == null || estudiante.getCorreo().trim().isEmpty()) {
             throw new Exception("El correo es obligatorio.");
         }
-        
+        if (estudiante.getContrasena() == null || estudiante.getContrasena().trim().isEmpty()) {
+            throw new Exception("La contraseña es obligatoria.");
+        }
         if (estudianteDAO.existeCorreo(estudiante.getCorreo())) {
             throw new Exception("El correo ingresado ya está registrado.");
         }
 
         return estudianteDAO.guardar(estudiante);
+    }
+
+    public List<Estudiante> listarPendientesDeEvaluar(Long idEmisor) {
+        return estudianteDAO.listarPendientesDeEvaluar(idEmisor);
+    }
+
+    public Optional<Estudiante> buscarPorId(Long id) {
+        return estudianteDAO.buscarPorId(id);
+    }
+
+    public List<Match> listarMatchesDeEstudiante(Long idEstudiante) {
+        return matchDAO.listarMatchesDeEstudiante(idEstudiante);
     }
 }
